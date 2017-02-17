fa8ed160815d82b9f64c62cf2c5d12bd21b9502c
david keller <dakeller@csumb.edu>
2017-02-17 02:53:14 +0000

    first

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import os
 import flask
 import flask_socketio
+import flask_sqlalchemy
+
 
 app = flask.Flask(__name__)
 socketio = flask_socketio.SocketIO(app)
 
+#import model
+
+
 @app.route('/')
 def hello():
-    return flask.render_template('index.html')
+    messages = model.Message.query.all()
+    html = ['<li>' + m.text + '</li>' for m in messages]
+    return '<ul>' + ''.join(html) + '</ul>'
+
+#    return flask.render_template('index.html')
 
 @socketio.on('connect')
 def on_connect():
@@ -17,15 +26,25 @@ def on_connect():
 def on_disconnect():
     print 'Someone disconnected!'
 
+all_numbers = []
 @socketio.on('new number')
 def on_new_number(data):
-    print "Got an event for new number with data:", data
-    # TODO: Fill me out!
+    all_numbers.append(data['number'])
+    socketio.emit('all numbers', {
+        'numbers': all_numbers
+    })
+test_list = []
+@socketio.on('text')
+def send_text(data):
+    test_list.append(data['value'])
+    socketio.emit('all test', {'tesxt': test_list})
+    
 
-socketio.run(
-    app,
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
-)
+if __name__ == '__main__':
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
+    )
 
--- scripts/Content.js
+++ scripts/Content.js
@@ -2,12 +2,15 @@ import * as React from 'react';
 
 import { Button } from './Button';
 import { Socket } from './Socket';
+import { NameForm } from './text';
+
 
 export class Content extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            'numbers': []
+            'numbers': [],
+            'text' : []
         };
     }
 
@@ -17,17 +20,28 @@ export class Content extends React.Component {
                 'numbers': data['numbers']
             });
         })
+        Socket.on('all test', (data) => {
+            this.setState({
+                'text':data['tesxt']
+            });
+        })
     }
 
     render() {
         let numbers = this.state.numbers.map(
             (n, index) => <li key={index}>{n}</li>
         );
+        let text = this.state.text.map(
+            (n,index) => <li key ={index}>{n}</li>
+            );
         return (
             <div>
-                <h1>Random numbers so far!</h1>
-                <ul>{numbers}</ul>
+                <h1>Randomvdds so far!</h1>
+                <ul>{numbers} {text}</ul>
                 <Button />
+                <NameForm />
+                
+                
             </div>
         );
     }
