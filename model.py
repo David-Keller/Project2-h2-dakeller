f3446d6e7e0a68e43e95ed50a824f18e62c5f4f3
david keller <dakeller@csumb.edu>
2017-02-17 03:14:24 +0000

    first

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import flask_sqlalchemy, app
+import flask_sqlalchemy, app, os
 #app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://potato:potatosareawesome@localhost/postgres'
 app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = flask_sqlalchemy.SQLAlchemy(app.app)
