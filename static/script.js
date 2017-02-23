3d8d2253cc835ad437b1d19fd29ad499289a9c31
david keller <dakeller@csumb.edu>
2017-02-23 19:12:07 +0000

    test

--- static/script.js
+++ static/script.js
@@ -13160,7 +13160,8 @@ var Content = exports.Content = function (_React$Component) {
                 React.createElement(
                     Image,
                     null,
-                    ' source="https://www.omnycontent.com/d/clips/0914dce6-85b4-4825-bcf4-a513005d374d/07e9c90d-6021-4325-a9f3-a514001d92a6/c3277bc7-f587-464a-9933-a53a00c84b3c/image.jpg?t=1445851979&download=True&utm_source=OmnyFM&size=Large"',
+                    ' source=',
+                    { uri: "https://www.omnycontent.com/d/clips/0914dce6-85b4-4825-bcf4-a513005d374d/07e9c90d-6021-4325-a9f3-a514001d92a6/c3277bc7-f587-464a-9933-a53a00c84b3c/image.jpg?t=1445851979&download=True&utm_source=OmnyFM&size=Large" },
                     React.createElement(
                         'h1',
                         null,
