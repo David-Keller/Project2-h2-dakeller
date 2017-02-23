15214e9aa2b1d3f64859bb76f93b01c9f1d422c0
david keller <dakeller@csumb.edu>
2017-02-23 21:55:38 +0000

    test

--- static/script.js
+++ static/script.js
@@ -13116,7 +13116,8 @@ var Content = exports.Content = function (_React$Component) {
         }
 
         // this line goes down in the render statement as soon at it is working
-        //             <BackgroundImg> img="https://www.omnycontent.com/d/clips/0914dce6-85b4-4825-bcf4-a513005d374d/07e9c90d-6021-4325-a9f3-a514001d92a6/c3277bc7-f587-464a-9933-a53a00c84b3c/image.jpg?t=1445851979&download=True&utm_source=OmnyFM&size=Large"
+        //                         <img src={"https://www.omnycontent.com/d/clips/0914dce6-85b4-4825-bcf4-a513005d374d/07e9c90d-6021-4325-a9f3-a514001d92a6/c3277bc7-f587-464a-9933-a53a00c84b3c/image.jpg?t=1445851979&download=True&utm_source=OmnyFM&size=Large"} />
+
 
     }, {
         key: 'render',
@@ -13157,40 +13158,36 @@ var Content = exports.Content = function (_React$Component) {
                 'div',
                 null,
                 React.createElement(
-                    'img',
-                    { src: "https://www.omnycontent.com/d/clips/0914dce6-85b4-4825-bcf4-a513005d374d/07e9c90d-6021-4325-a9f3-a514001d92a6/c3277bc7-f587-464a-9933-a53a00c84b3c/image.jpg?t=1445851979&download=True&utm_source=OmnyFM&size=Large" },
-                    React.createElement(
-                        'h1',
-                        null,
-                        'Wellcome to the message board :-)'
-                    ),
-                    React.createElement('div', {
-                        className: 'fb-login-button',
-                        'data-max-rows': '1',
-                        'data-size': 'medium',
-                        'data-show-faces': 'false',
-                        'data-auto-logout-link': 'true' }),
-                    React.createElement(
-                        'h1',
-                        null,
-                        'Number of users: ',
-                        num
-                    ),
-                    React.createElement(
-                        'h1',
-                        null,
-                        'Messages so far!'
-                    ),
-                    React.createElement(
-                        'ul',
-                        null,
-                        numbers,
-                        ' ',
-                        message
-                    ),
-                    React.createElement(_Button.Button, null),
-                    React.createElement(_text.NameForm, null)
-                )
+                    'h1',
+                    null,
+                    'Wellcome to the message board :-)'
+                ),
+                React.createElement('div', {
+                    className: 'fb-login-button',
+                    'data-max-rows': '1',
+                    'data-size': 'medium',
+                    'data-show-faces': 'false',
+                    'data-auto-logout-link': 'true' }),
+                React.createElement(
+                    'h1',
+                    null,
+                    'Number of users: ',
+                    num
+                ),
+                React.createElement(
+                    'h1',
+                    null,
+                    'Messages so far!'
+                ),
+                React.createElement(
+                    'ul',
+                    null,
+                    numbers,
+                    ' ',
+                    message
+                ),
+                React.createElement(_Button.Button, null),
+                React.createElement(_text.NameForm, null)
             );
         }
     }]);
